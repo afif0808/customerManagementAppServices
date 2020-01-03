7622a78983abdb8abc407ca2155479390907d370
root <afif.panai@gmail.com>
2020-01-03 09:23:54 +0700

    another commit

--- controllers/GetCustomersController.go
+++ controllers/GetCustomersController.go
@@ -11,13 +11,12 @@ import (
 func GetCustomersController(getCustomer models.GetCustomersFunc, searchCustomers models.SearchCustomersFunc) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// see if limit is defined or is '0'
-		// var limit will be '0' if it's not defined
-		// if limit is not defined then 'strconv.Atoi' will return error and var limit is zero
-		limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
-		if limit == 0 {
-			// default limit
-			limit = 10
+		// get 'limit' url query and parse it into integer.
+		// if error is found then client 'limit' probably not defined by client
+		// or the type is not valid so default value is assigned instead.
+		limit, parseIntErr := strconv.Atoi(r.URL.Query().Get("limit"))
+		if parseIntErr != nil {
+			limit = 10 // '10' is default value
 		}
 
 	})
