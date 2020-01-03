39703e502dda3e270b3affb04aa6c056d80b9a82
root <afif.panai@gmail.com>
2020-01-03 08:33:07 +0700

    another commit

--- controllers/GetCustomersController.go
+++ controllers/GetCustomersController.go
@@ -1,12 +1,24 @@
 package controllers
 
-import "net/http"
-import "customerManagementAppServices/models"
+import (
+	"customerManagementAppServices/models"
+	"net/http"
+	"strconv"
+)
 
 // GetCustomersController search for customers by name if parameter url parameter 'q' is defined
 // otherwise , it serves list of customers
 func GetCustomersController(getCustomer models.GetCustomersFunc, searchCustomers models.SearchCustomersFunc) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// see if limit is defined or is '0'
+		// var limit will be '0' if it's not defined
+		// if limit is not defined then 'strconv.Atoi' will return error and var limit is zero
+		limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+		if limit == 0 {
+			// default limit
+			limit = 10
+		}
 
 	})
 }
--- controllers/GetCustomersController_test.go
+++ controllers/GetCustomersController_test.go
@@ -10,5 +10,9 @@ import (
 func TestGetCustomerController(t *testing.T) {
 	testRecoder := httptest.NewRecorder()
 	testRouter := mux.NewRouter()
-	testRequest := httptest.NewRequest("GET", "http://localhost:378/customers", nil)
+	testRouter.Handle("/customers", GetCustomersController(nil, nil))
+	testGetCustomers := httptest.NewRequest("GET", "http://localhost:378/customers", nil)
+	testSearchCustomers := httptest.NewRequest("GET", "http://localhost:378/customers?q=fulan", nil)
+	testPaging := httptest.NewRequest("GET", "http://localhost:378/customers?limit=10,afterId=5", nil)
+
 }
