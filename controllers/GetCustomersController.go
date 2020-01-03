4fe219b4bc23cbf97129817bbf57411da04edf3e
root <afif.panai@gmail.com>
2020-01-03 17:05:20 +0700

    another commit

--- controllers/GetCustomersController.go
+++ controllers/GetCustomersController.go
@@ -8,15 +8,16 @@ import (
 
 // GetCustomersController search for customers by name if parameter url parameter 'q' is defined
 // otherwise , it serves list of customers
+// implements 'Seek Pagination' method.
 func GetCustomersController(getCustomer models.GetCustomersFunc, searchCustomers models.SearchCustomersFunc) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// get 'limit' url query and parse it into integer.
+		// get 'limit' url query and parse it to be an integer.
 		// if error is found then client 'limit' probably not defined by client
-		// or the type is not valid so default value is assigned instead.
-		limit, parseIntErr := strconv.Atoi(r.URL.Query().Get("limit"))
+		// or the type is not valid  , so default value is assigned instead.
+		customerlimit, parseIntErr := strconv.Atoi(r.URL.Query().Get("limit"))
 		if parseIntErr != nil {
-			limit = 10 // '10' is default value
+			customerlimit = 10 // '10' is default value
 		}
 
 	})
--- controllers/GetCustomersController_test.go
+++ controllers/GetCustomersController_test.go
@@ -1,19 +1,15 @@
 package controllers
 
 import (
-	"customerManagementAppServices/models"
-	"net/http/httptest"
 	"testing"
-
-	"github.com/gorilla/mux"
 )
 
 func TestGetCustomerController(t *testing.T) {
-	testRecoder := httptest.NewRecorder()
-	testRouter := mux.NewRouter()
-	testRouter.Handle("/customers", GetCustomersController(models.GetCustomersFunc{}, models.SearchCustomersFunc))
-	testGetCustomers := httptest.NewRequest("GET", "http://localhost:378/customers", nil)
-	testSearchCustomers := httptest.NewRequest("GET", "http://localhost:378/customers?q=fulan", nil)
-	testPaging := httptest.NewRequest("GET", "http://localhost:378/customers?limit=10,afterId=5", nil)
+	// testRecoder := httptest.NewRecorder()
+	// testRouter := mux.NewRouter()
+	// // testRouter.Handle("/customers", GetCustomersController(models.GetCustomersFunc{}, models.SearchCustomersFunc{}))
+	// testGetCustomers := httptest.NewRequest("GET", "http://localhost:378/customers", nil)
+	// testSearchCustomers := httptest.NewRequest("GET", "http://localhost:378/customers?q=fulan", nil)
+	// testPaging := httptest.NewRequest("GET", "http://localhost:378/customers?limit=10,afterId=5", nil)
 
 }
