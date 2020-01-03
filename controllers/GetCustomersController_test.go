9e11ad267c6275c251822c49dc00d28f1a4950da
root <afif.panai@gmail.com>
2020-01-03 09:25:38 +0700

    anohter commit

--- controllers/GetCustomersController_test.go
+++ controllers/GetCustomersController_test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"customerManagementAppServices/models"
 	"net/http/httptest"
 	"testing"
 
@@ -10,7 +11,7 @@ import (
 func TestGetCustomerController(t *testing.T) {
 	testRecoder := httptest.NewRecorder()
 	testRouter := mux.NewRouter()
-	testRouter.Handle("/customers", GetCustomersController(nil, nil))
+	testRouter.Handle("/customers", GetCustomersController(models.GetCustomersFunc{}, models.SearchCustomersFunc))
 	testGetCustomers := httptest.NewRequest("GET", "http://localhost:378/customers", nil)
 	testSearchCustomers := httptest.NewRequest("GET", "http://localhost:378/customers?q=fulan", nil)
 	testPaging := httptest.NewRequest("GET", "http://localhost:378/customers?limit=10,afterId=5", nil)
