54b8a6568810eb6d113a646f11d163628fbc18d2
root <afif.panai@gmail.com>
2020-01-06 08:14:33 +0700

    another coomit

--- controllers/GetCustomersController.go
+++ controllers/GetCustomersController.go
@@ -9,16 +9,31 @@ import (
 // GetCustomersController search for customers by name if parameter url parameter 'q' is defined
 // otherwise , it serves list of customers
 // implements 'Seek Pagination' method.
-func GetCustomersController(getCustomer models.GetCustomersFunc, searchCustomers models.SearchCustomersFunc) http.Handler {
+func GetCustomersController(getCustomers models.GetCustomersFunc, searchCustomers models.SearchCustomersFunc) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// get 'limit' url query and parse it to be an integer.
+		// get 'limit' url parameter value and parse it to be an integer.
 		// if error is found then client 'limit' probably not defined by client
 		// or the type is not valid  , so default value is assigned instead.
-		customerlimit, parseIntErr := strconv.Atoi(r.URL.Query().Get("limit"))
+		customerLimit, parseIntErr := strconv.Atoi(r.URL.Query().Get("limit"))
 		if parseIntErr != nil {
-			customerlimit = 10 // '10' is default value
+			customerLimit = 10 // '10' is default value
 		}
 
+		// get 'afterId' url parameter value and parse it to be an integer
+		// if 'afterId' is not given or it's not string number , the  integer conversion will return an error
+		// and afterId variable remains zero since it's the default value of integer
+		afterId, _ := strconv.Atoi(r.URL.Query().Get("afterId"))
+
+		searchQuery := r.URL.Query().Get("q")
+
+		var result []models.CustomerModel
+		var searchCustomersError error
+		var getCustomersError error
+		if searchQuery == "" {
+			result, getCustomersError = getCustomers(customerLimit, afterId)
+		} else {
+			result, searchCustomersError = searchCustomers(customerLimit, afterId, searchQuery)
+		}
 	})
 }
